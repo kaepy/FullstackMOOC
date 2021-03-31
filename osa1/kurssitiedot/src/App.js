3e4e213ddaf44bbe6823876e2c10661cd65d24a0
kaepy <henna.tuunainen@gmail.com>
2021-03-31 22:41:41 +0300

    Add Osa1 tehtävä 1.5 kurssitiedot - array olion sisällä

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <Content course={course} />
+      <Total course={course}/>
     </div>
   )
 }
@@ -29,7 +31,7 @@ const Header = (props) => {
   console.log(props)
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -51,9 +53,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.parts[0].name} />
-      <Part part={props.parts[1].name} />
-      <Part part={props.parts[2].name} />
+      <Part part={props.course.parts[0].name} />
+      <Part part={props.course.parts[1].name} />
+      <Part part={props.course.parts[2].name} />
     </div>
   )
 }
@@ -62,7 +64,7 @@ const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[0].exercises + props.parts[0].exercises}</p>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[0].exercises + props.course.parts[0].exercises}</p>
     </div>
   )
 }
