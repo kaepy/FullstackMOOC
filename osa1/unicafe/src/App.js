44d46e0b955812fbb32bcd76ed29d68be67fe087
kaepy <henna.tuunainen@gmail.com>
2022-01-19 14:38:06 +0200

    Add 1.6 unicafe step 1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+// 1.6: unicafe step1
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
+import React, { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h2>statistics</h2>
+      <p>
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}
+      </p>
+    </div>
+  )
+}
+
+// Uusiokäytettävä nappi
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
 export default App
