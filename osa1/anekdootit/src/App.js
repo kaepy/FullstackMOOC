70d50d7ee0aa0c4f25ca832e261c236da6db7b1d
kaepy <henna.tuunainen@gmail.com>
2022-01-20 22:29:24 +0200

    Add 1.13* anekdootit step 2

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,5 +1,6 @@
-// 1.12*: anekdootit step 1
-// Laajenna seuraavaa sovellusta siten, että siihen tulee nappi, jota painamalla sovellus näyttää satunnaisen ohjelmistotuotantoon liittyvän anekdootin.
+// 1.13*: anekdootit step 2
+// Laajenna sovellusta siten, että näytettävää anekdoottia on mahdollista äänestää. 
+// Huom: kunkin anekdootin äänet kannattanee tallettaa komponentin tilassa olevan olion kenttiin tai taulukkoon. Muista, että tilan oikeaoppinen päivittäminen edellyttää olion tai taulukon kopioimista.
 
 import React, { useState } from 'react'
 
@@ -13,19 +14,28 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
+  
   const [selected, setSelected] = useState(0)
 
+  // komponentin tilassa oleva olio
+  const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 })
 
+  // palauttaa random integerin 0 ja 6 väliltä
   const handleRandom = () => {
-    // // palauttaa random integerin 0 ja 6 väliltä
     setSelected(Math.floor(Math.random() * anecdotes.length))
-    console.log(selected)
+  }
+
+  const handleVote = () => {
+    const copy = { ...points } // kopioidaan olio.
+    copy[selected] += 1   // kasvatetaan selectedin mukaisen olion kentän arvoa yhdellä
+    setPoints(copy)
   }
 
   return (
     <div>
       {anecdotes[selected]} <br/>
+      has {points[selected]} votes <br/>
+      <Button handleClick={handleVote} text='vote' />
       <Button handleClick={handleRandom} text='next anecdote' />
     </div>
   )
