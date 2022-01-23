4470649b034daa0de834f324bcd4a24bb25a3a79
kaepy <henna.tuunainen@gmail.com>
2022-01-23 17:59:17 +0200

    Add 2.4 kurssitiedot step 9

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,50 +1,69 @@
-// 2.3*: kurssitiedot step 8
-
-// Pro tip 1: Nuolifunktion muuttaminen pidempään muotoon console.login käyttöä varten map/reduce metodien kanssa
-// Pro tip 2: VS Codeen on asennettavissa laajennus, jonka avulla nuolifunktion lyhyen muodon voi muuttaa automaattisesti pidemmäksi muodoksi ja päinvastoin
+// 2.4: kurssitiedot step 9
+// Laajennetaan sovellusta siten, että kursseja voi olla mielivaltainen määrä
 
 import React from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  //console.log(props)
+  console.log('App toimii...')
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => console.log('Map', course) ||
+        <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
 
 // kurssin muotoilu
 const Course = (props) => {
-  //console.log(props)
+  console.log('Course', props)
+
   return (
     <div>
       <Header course={props.course} />
@@ -55,7 +74,7 @@ const Course = (props) => {
 
 // otsikon näyttäminen
 const Header = (props) => {
-  //console.log(props)
+  console.log('Header', props)
   return (
     <div>
       <h1>{props.course.name}</h1>
@@ -65,10 +84,9 @@ const Header = (props) => {
 
 // kurssin sisällön muotoilu ja harjoitusten totaalin laskenta
 const Content = (props) => {
-  //console.log(props)
+  console.log('Content', props)
 
-  // Pro tip 3: Mikäli console.login haluaa vain pikaisesti ujuttaa koodiin nuolifunktiota muuttamatta
-  const totalExercises = props.course.parts.reduce((sum, part) => console.log('hello', sum, part) || sum + part.exercises, 0)
+  const totalExercises = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
     <div>
@@ -82,7 +100,7 @@ const Content = (props) => {
 
 // osien näyttäminen
 const Part = (props) => {
-  //console.log(props)
+  //console.log('Part', props)
   return (
       <p>{props.part} {props.exercises}</p>
   )
@@ -90,7 +108,7 @@ const Part = (props) => {
 
 // totaalin näyttäminen
 const Total = (props) => {
-  //console.log(props)
+  //console.log('Total', props)
   return (
     <div>
       <p><b>total of {props.totalExercises} exercises</b></p>
