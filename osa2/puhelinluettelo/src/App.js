d0212954f65c7b84ec6bdc04ac2bc089edc6ddfa
kaepy <henna.tuunainen@gmail.com>
2022-08-27 00:53:23 +0300

    Add 2.9 puhelinluettelo step 4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,9 +5,17 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = (props) => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas', number: '040-1231244' } ]) // puhelinluettelon persoonat
+  const [persons, setPersons] = useState([ 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) // puhelinluettelon persoonat
+
   const [newName, setNewName] = useState('') // lomakkeen syöte
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  //const [showAll, setShowAll] = useState(true)
   //console.log('persons', persons)
   //console.log('newName', newName)
   //console.log('newNumber', newNumber)
@@ -46,11 +54,30 @@ const App = (props) => {
     setNewNumber(event.target.value)
   }
 
+  const handleNewFilter = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  const personsToShow = !newFilter //ehto
+    ? persons // true
+    // luodaan uusi array jossa on kaikki 
+    : persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase())) // false, luo uuden arrayn
+
   console.log('App toimii...')
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with <input value={newFilter} onChange={handleNewFilter} />
+      </div>
+      
+        {/*</div>() => setShowAll(!showAll) //muuta showAll falseksi
+          show {showAll ? 'important' : 'all' }*/}
+
+
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -62,13 +89,16 @@ const App = (props) => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <div> 
-        {persons.map(person => console.log('Map', person) ||
+        //nyt ei haluta käydä kaikkia läpi vaan ainoastaan showAll tai filteröity uusi array
+        {personsToShow.map(person => console.log('Map', person) ||
           <Person key={person.name} person={person} />
           )}
       </div>
-       <div>debug: {newName} {newNumber}</div> 
+       {/*<div>debug: {newName} {newNumber}</div>*/}
+       <div>debug: {newFilter}</div>
     </div>
   )
 
