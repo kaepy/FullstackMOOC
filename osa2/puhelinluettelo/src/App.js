b15ee401f6a4ccfa2e7b6e8d41de6f5ffedbb669
kaepy <henna.tuunainen@gmail.com>
2022-08-26 15:40:07 +0300

    Add 2.7 puhelinluettelo step 2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,24 +2,34 @@
 // Toteutetaan henkilön lisäys puhelinluetteloon
 
 import React, { useState } from 'react'
+import Person from './components/Person'
 
-const App = () => {
-  
+const App = (props) => {
   const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) // puhelinluettelon persoonat
   const [newName, setNewName] = useState('') // lomakkeen syöte
-  
+  console.log('persons', persons)
+  console.log('newName', newName)
 
   // uuden henkilön lisääminen
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
-    const personObject = {
-      name: newName,
+    // obj->obj vertailu ei ole mahdollista, koska ei tiedetä mitä vertailla
+    // tästä syystä vertaillaan obj name tietoja keskenään
+    if (persons.some(person => person.name === newName)){
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      const personObject = {
+      name: newName
+      }
+
+      setPersons(persons.concat(personObject)) // Uusi persoona lisätään vanhojen joukkoon
+      setNewName('') /// Tyhjennetään syötekenttää kontrolloiva olio
     }
 
-    setPersons(persons.concat(personObject)) // Uusi persoona lisätään vanhojen joukkoon
-    setNewName('')
+    //setNewName tyhjennys ei tapahdu tässä jottei typon takia tyhjennetä kenttää
+
   }
 
   const handleNameChange = (event) => {
@@ -27,13 +37,7 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const Person = ({ person }) => {
-    return (
-      <li>{person.name}</li>
-    )
-  }
-
-  //console.log('App toimii...')
+  console.log('App toimii...')
 
   return (
     <div>
@@ -47,8 +51,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div> {persons.map(person => <Person key={person.name} person={person} />)}</div>
-      <div>debug: {newName}</div>
+      <div> 
+        {persons.map(person => console.log('Map', person) ||
+          <Person key={person.name} person={person} />
+          )}
+      </div>
+      /* <div>debug: {newName}</div> */
     </div>
   )
 
